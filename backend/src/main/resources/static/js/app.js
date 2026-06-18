1269af8eb7719f57e98dbc6cfc944f454ed89792
stevenMarmion <marmionsteven07@gmail.com>
2026-06-18 11:23:31 +0200

    [CODE] : Delete no longer usefull static interfaces from backend and add cors configuration from backend to frotnend

--- backend/src/main/resources/static/js/app.js
+++
@@ -1,5 +0,0 @@
-import { get_api, post_api, put_api, delete_api } from "./service/api.js";
-
-const secret_config = await get_api('/api/config');
-
-console.log(`Secret config value: ${secret_config}`);
